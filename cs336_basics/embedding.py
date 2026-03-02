5f8ce5215bea92d13e5a2d8d1f66bc0035dd5221
AndyShaw01 <974052646@qq.com>
2026-03-02 22:39:50 +0800

    feat(embedding): add Embedding class and integrate with run_embedding function

---
+++ cs336_basics/embedding.py
@@ -0,0 +1,18 @@
+import torch
+import math
+
+class Embedding(torch.nn.Module):
+    def __init__(self, num_embeddings, embedding_dim, device=None, dtype=None):
+        super().__init__()
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = embedding_dim
+        self.weights = torch.nn.Parameter(torch.randn(num_embeddings, embedding_dim, device=device, dtype=dtype))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        std = 1
+        torch.nn.init.trunc_normal_(self.weights, mean=0.0, a=-3*std, b=3*std)
+
+    def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
+        token_embs = self.weights[token_ids]
+        return token_embs
