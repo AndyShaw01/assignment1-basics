c2b1ed6cd40152dba3646f53bc7f83cf447f35ac
AndyShaw01 <974052646@qq.com>
2026-03-04 00:15:48 +0800

    feat(rmsnorm): implement RMSNorm class and integrate with run_rmsnorm function

--- cs336_basics/rmsnorm.py
+++ cs336_basics/rmsnorm.py
@@ -1,2 +1,14 @@
 import torch
-import math
+
+class RMSNorm(torch.nn.Module):
+    def __init__(self, d_model, eps=1e-5, device=None, dtype=None):
+        super().__init__()
+        self.g = torch.nn.Parameter(torch.ones(d_model, device=device, dtype=dtype))
+        self.eps = eps
+    
+    def forward(self, x):
+        in_dtype = x.dtype
+        x = x.to(torch.float32) # [B, T, D]
+        rms = torch.sqrt(x.pow(2).mean(dim=-1, keepdim=True) + self.eps) # [B, T, 1]
+        result = (x / rms) * self.g
+        return result.to(in_dtype)
