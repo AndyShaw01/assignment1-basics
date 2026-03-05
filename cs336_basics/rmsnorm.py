5168f3c8b4f7290e3d50e7d0b88ebcf8683114a2
AndyShaw01 <974052646@qq.com>
2026-03-05 15:31:36 +0800

    fix(rmsnorm): rename variable from g to weight for consistency in RMSNorm class

--- cs336_basics/rmsnorm.py
+++ cs336_basics/rmsnorm.py
@@ -3,12 +3,12 @@ import torch
 class RMSNorm(torch.nn.Module):
     def __init__(self, d_model, eps=1e-5, device=None, dtype=None):
         super().__init__()
-        self.g = torch.nn.Parameter(torch.ones(d_model, device=device, dtype=dtype))
+        self.weight = torch.nn.Parameter(torch.ones(d_model, device=device, dtype=dtype)) # g
         self.eps = eps
     
     def forward(self, x):
         in_dtype = x.dtype
         x = x.to(torch.float32) # [B, T, D]
         rms = torch.sqrt(x.pow(2).mean(dim=-1, keepdim=True) + self.eps) # [B, T, 1]
-        result = (x / rms) * self.g
+        result = (x / rms) * self.weight
         return result.to(in_dtype)
