969b1ba4094fefbd05d8c913a5b7a8e0eba3947a
AndyShaw01 <974052646@qq.com>
2026-03-05 11:47:07 +0800

    feat(multihead_self_attention): add RoPE integration and update constructor to accept theta parameter

--- cs336_basics/multihead_self_attention.py
+++ cs336_basics/multihead_self_attention.py
@@ -3,9 +3,10 @@ import torch
 from einops import einsum
 from cs336_basics.linear import Linear
 from cs336_basics.softmax import Softmax
+from cs336_basics.rope import RoPE
 
 class MultiHeadSelfAttention(torch.nn.Module):
-    def __init__(self, d_model, num_heads, max_seq_length, device=None):
+    def __init__(self, d_model, num_heads, max_seq_length, theta=None, device=None):
         super().__init__()
         assert d_model % num_heads == 0, "d_model must be divisible by num_heads"
         self.d_model = d_model
@@ -16,13 +17,15 @@ class MultiHeadSelfAttention(torch.nn.Module):
         self.w_v = Linear(self.d_model, self.d_model, device=device)
         self.w_o = Linear(self.d_model, self.d_model, device=device)
         self.softmax = Softmax()
+        if theta:
+            self.rope = RoPE(theta, self.head_dim, max_seq_length, device=device)
         mask = torch.triu(
             torch.ones(max_seq_length, max_seq_length, device=device, dtype=torch.bool), 
             diagonal=1
         ) # future token is true
         self.register_buffer("mask", mask, persistent=False) # deterministic, in the forward function, we can call the "mask" by "self.mask"
 
-    def forward(self, x):
+    def forward(self, x, using_rope=False, token_positions=None):
         B, T, _ = x.shape
         # [B, T, D] -> [B, T, D]
         queries = self.w_q(x)
@@ -32,6 +35,9 @@ class MultiHeadSelfAttention(torch.nn.Module):
         queries = queries.view(B, T, self.num_heads, self.head_dim).transpose(1, 2)
         keys = keys.view(B, T, self.num_heads, self.head_dim).transpose(1, 2)
         values = values.view(B, T, self.num_heads, self.head_dim).transpose(1, 2)
+        if using_rope:
+            queries = self.rope(queries, token_positions)
+            keys = self.rope(keys, token_positions)
         # attn_scores
         attn_scores = einsum(queries, keys, "... q d_in, ... k d_in -> ... q k") / self.head_dim ** 0.5
         mask = self.mask[:T, :T]
