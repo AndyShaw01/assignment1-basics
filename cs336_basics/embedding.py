b0303d11f749549c777b544f5689f69be2c07ad6
AndyShaw01 <974052646@qq.com>
2026-03-05 15:31:21 +0800

    fix(embedding): correct variable name from weights to weight in Embedding class

--- cs336_basics/embedding.py
+++ cs336_basics/embedding.py
@@ -6,13 +6,13 @@ class Embedding(torch.nn.Module):
         super().__init__()
         self.num_embeddings = num_embeddings
         self.embedding_dim = embedding_dim
-        self.weights = torch.nn.Parameter(torch.randn(num_embeddings, embedding_dim, device=device, dtype=dtype))
+        self.weight = torch.nn.Parameter(torch.randn(num_embeddings, embedding_dim, device=device, dtype=dtype))
         self.reset_parameters()
 
     def reset_parameters(self):
         std = 1
-        torch.nn.init.trunc_normal_(self.weights, mean=0.0, a=-3*std, b=3*std)
+        torch.nn.init.trunc_normal_(self.weight, mean=0.0, a=-3*std, b=3*std)
 
     def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
-        token_embs = self.weights[token_ids]
+        token_embs = self.weight[token_ids]
         return token_embs
